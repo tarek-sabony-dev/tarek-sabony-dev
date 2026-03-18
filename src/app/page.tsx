e6f4c2e57728137bde3a6889a430962f52e3f878
tarek-sabony-dev <tareksabony@gmail.com>
2026-03-18 17:01:05 +0300

    added scroll progress bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,22 @@ import LoadingAnimation from "@/components/loading";
 import HomePage from "@/components/pages/home";
 import { TextAnimate } from "@/components/ui/text-animate";
 import { useEffect, useState } from "react";
-import { motion, useScroll } from 'framer-motion'
+import { motion, useScroll, useTransform } from 'framer-motion'
 import { Code2 } from "lucide-react";
 
 export default function Home () {
   const [showLoading, setShowLoading] = useState(true)
+  const [displayPercentage, setDisplayPercentage] = useState(0)
   const { scrollYProgress } = useScroll()
+  const width = useTransform(scrollYProgress, [0, 1], ['0%', '100%'])
+
+  useEffect(() => {
+    const unsubscribe = scrollYProgress.on('change', (latest) => {
+      setDisplayPercentage(Math.round(latest * 100));
+    });
+    
+    return () => unsubscribe();
+  }, [scrollYProgress])
 
   useEffect(() => {
     // Hide after animation (8s delay + 0.5s fade = 8.5s)
@@ -17,17 +27,20 @@ export default function Home () {
     return () => clearTimeout(timeout)
   }, [])
 
-  if(showLoading) return <LoadingAnimation />
+  // if(showLoading) return <LoadingAnimation />
 
   return (
     <main className="bg-[#e8e8e3] flex flex-col relative">
       <HomePage/>
-      <div className="w-full h-375 bg-[#080807] z-10">
-        saddddddd
-      </div>
+      <motion.div className="w-full h-575 text-white bg-[#080807] z-10">
+        {displayPercentage}
+      </motion.div>
+      <motion.div className="w-full h-575 text-white bg-[#E8E8E3] z-10">
+        {displayPercentage}
+      </motion.div>
 
 
-      <TextAnimate 
+      <TextAnimate
         className="w-fit h-fit fixed bottom-4 left-4 text-xs sm:text-base text-[#e8e8e3] font-medium font-[lora] mix-blend-difference z-10"
         animation="slideUp"
         by="text"
@@ -38,9 +51,10 @@ export default function Home () {
         Think it. Solve it. Build it.
       </TextAnimate>
       <motion.div
-        initial={{ y: -10, opacity: 0 }}
+        initial={{ y: -20, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
-        transition={{ delay: 0.5, duration: 0.5, ease: "easeInOut" }}
+        exit={{ y: 20, opacity: 0 }}
+        transition={{ delay: 0.5, duration: 0.3, ease: "easeOut" }}
         className="w-fit h-fit fixed top-6 right-6 mix-blend-difference z-10"
       >
         <Code2 size={28} color="#e8e8e3" />
@@ -48,6 +62,24 @@ export default function Home () {
       <TextAnimate animation="slideLeft" by="text" duration={1.5} delay={0.5} once className="w-fit hidden sm:inline-block fixed top-1/4 right-6 text-3xl text-[#e8e8e3] text-center font-medium font-[raleway] leading-12 mix-blend-difference z-10" >
         {`D\nE\nV\nE\nL\nO\nP\nE\nR`}
       </TextAnimate>
+      <motion.div
+        initial={{ y: 20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: -20, opacity: 0 }}
+        transition={{ delay: 0.5, duration: 0.3, ease: "easeOut" }}
+        className="w-fit h-fit flex justify-end items-center gap-0 sm:gap-2 fixed bottom-4 right-4 text-xs sm:text-base text-[#e8e8e3] font-medium font-[lora] mix-blend-difference z-10"
+      >
+        <div className="w-25 sm:w-50 h-0.5 flex justify-start bg-[#6B645C]/50">
+          <motion.div 
+            className="w-full h-full bg-[#e8e8e3] "
+            style={{ scaleX: width }}
+            >
+          </motion.div>
+        </div>
+        <span className="w-10 text-right">
+          {`${displayPercentage}%`}
+        </span>
+      </motion.div>
     </main>
   );
 }
