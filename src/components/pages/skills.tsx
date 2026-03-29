e8d8805e4397ec553f1d48db6188d2d10c4db386
tarek-sabony-dev <tareksabony@gmail.com>
2026-03-29 15:40:33 +0300

    fixing scroll reveal animation

--- src/components/pages/skills.tsx
+++ src/components/pages/skills.tsx
@@ -1,15 +1,15 @@
 "use client"
 
 import { MotionValue } from 'motion';
-import ScrollRevealText from '../ui/scroll-reveal-text';
+import { TextReveal } from '../ui/scroll-text-reveal';
 
 export default function SkillsPage ({ scroll } : { scroll : MotionValue}) {
   
   return (
     <div className="w-full flex flex-col justify-center items-center bg-dark z-10" >
-      <ScrollRevealText height='500vh'>
-        What I Do
-      </ScrollRevealText>
+      <TextReveal>
+        I build fast, modern websites that help businesses grow, available for freelance projects worldwide.
+      </TextReveal>
     </div>
   )
 }
